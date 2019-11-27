caed3b5bbc49370132312c89d6eafa916a36fae4
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-27 11:39:39 +0100

    add @featured_campaign to campaigns#index

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,5 +1,6 @@
 class CampaignsController < ApplicationController
   def index
     @campaigns = policy_scope(Campaign)
+    @featured_campaign = Campaign.find_by organisation: 'Fridays for Future'
   end
 end
