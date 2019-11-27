2196e5f5a036af0fd9593aefc0cce016e7be7258
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-27 15:08:25 +0100

    add selections table to the database

---
+++ db/migrate/20191127140645_create_selections.rb
@@ -0,0 +1,8 @@
+class CreateSelections < ActiveRecord::Migration[5.2]
+  def change
+    create_table :selections do |t|
+      t.references :user, foreign_key: true
+      t.references :campaign, foreign_key: true
+    end
+  end
+end
