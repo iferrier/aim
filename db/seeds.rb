d6ff51d2247c7b551432fc2f1fb7b818a2c0903f
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-27 15:00:40 +0100

    fixed minor bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,6 @@ Campaign.create([
     title: 'Fridays for Future @ Berlin',
     campaign_description: 'March for a cleaner future!',
     cause: 'Climate Change',
-    campaign_picture: 'https://www.maz-online.de/var/storage/images/rnd/nachrichten/politik/fridays-for-future-wie-die-schueler-europa-ihren-stempel-aufdruecken/720657963-3-ger-DE/Fridays-for-Future-Wie-die-Schueler-Europa-ihren-Stempel-aufdruecken_big_teaser_article.jpg',
     location: 'Berlin',
     organisation: 'Fridays for Future'
   },
@@ -51,14 +50,12 @@ Campaign.create([
     title: 'STOP violence against women',
     campaign_description: 'Stand up - from women, for women',
     cause: 'Women Equality',
-    campaign_picture: 'https://www.awarenessdays.com/wp-content/uploads/2018/07/stopviolence-1200x720-1024x614.jpg',
     launch_date: '2019_11_25'
   },
   {
     title: 'Who made my clothes?',
     campaign_description: 'Raise awareness on clothes production',
     cause: 'Equality and development',
-    campaign_picture: 'https://saubere-kleidung.de/wp-content/uploads/2018/01/who-made-my-clothes-fashion-revolution-germany-350x350.jpg',
     organisation: 'Clean Clothes Campaign'
   }
 ])
