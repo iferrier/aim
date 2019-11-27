60debf1cfeb4460aba82c80278b0ad9ae085a931
Susie Kim <hello@susiekim.de>
2019-11-27 18:27:54 +0100

    footer and start of how we do it box

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,21 @@
+//= require jquery
 //= require rails-ujs
 //= require_tree .
+
+
+// const content = document.querySelector('.content');
+// const link2 = document.querySelector('.link-t');
+
+// console.log(link2)
+
+// link2.addEventListener('click', (event) => {
+//   event.preventDefault()
+//   console.log(event);
+//   if (content.classList.contains('d-none')) {
+//     link2.innerText = 'Read Less'
+//     content.classList.remove('d-none')
+//   } else {
+//     link2.innerText = 'Read More'
+//     content.classList.add('d-none')
+//   }
+// })
