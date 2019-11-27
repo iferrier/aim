7aab9d49fe7d237b48d0f86bea6066f0411790e4
Paola Barboglio <paola.barboglio@gmail.com>
2019-11-27 11:20:56 +0100

    add user seeds to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,36 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Cleaning database..."
+Campaign.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+
+User.create ([
+  {
+    name: 'Princess Carolyn',
+    email: 'pcarolyn@example.com',
+    password: '123123',
+    bio: 'I am Princess Carolyn - nothing to add!',
+    picture: 'https://vignette.wikia.nocookie.net/bojackhorseman/images/d/de/Princess_Carolyn.png/revision/latest?cb=20171017002131'
+  },
+  {
+    name: 'Bojack Horseman',
+    email: 'bojack@example.com',
+    password: '123123',
+    bio: 'I spend a lot of time with the real me and believe me, nobody is going to love that guy.',
+    picture: 'https://cdn.vox-cdn.com/thumbor/kJTD0f73ITuO7VM_c5PlaP03LP0=/0x0:3840x2160/1200x800/filters:focal(1613x773:2227x1387)/cdn.vox-cdn.com/uploads/chorus_image/image/61420935/BoJack_Horseman_S05E01_21m31s30960f.0.jpg'
+  },
+  {
+    name: 'Diane Nguyen',
+    email: 'diane@example.com',
+    password: '123123',
+    bio: 'You are responsible for your own happiness.',
+    picture: 'https://dazedimg-dazedgroup.netdna-ssl.com/900/azure/dazed-prod/1240/9/1249781.jpg'
+  }
+])
+
 puts "Creating campaigns..."
 
 Campaign.create([
