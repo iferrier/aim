1935cd62ec8f6c1468b3d9c214189e8444b1f2a0
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-12-02 12:12:22 +0100

    Display comments with avatar, user name and created date done in ruby

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module Aim
     config.load_defaults 5.2
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    config.time_zone = 'Berlin'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
