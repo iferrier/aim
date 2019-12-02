d483822d90f4f93ed48d6f14bf22a0b76baa9a26
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-12-02 18:31:19 +0100

    Done Styling of campaign cards, aligned with the other elements on the page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,4 @@
 // = require_tree .
 
 
-const content = document.querySelector('.content-how');
-const link2 = document.querySelector('.link-t');
-
-link2.addEventListener('click', (event) => {
-  event.preventDefault()
-  if (content.classList.contains('d-none')) {
-    link2.innerText = '--'
-    content.classList.remove('d-none')
-  } else {
-    link2.innerText = '+'
-    content.classList.add('d-none')
-  }
-})
-
-
-
 
